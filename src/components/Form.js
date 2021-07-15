a77b25b3886b5bb9148d4f2e5ef51cabafefe1ce
gfstee <georg.treitz@gmail.com>
2021-07-15 09:40:37 +0200

    fix

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,12 @@
 const Form = () => {
     return (
         <div className="form">
-            <form method="post" name="contact">
+            <form
+                method="post"
+                name="contact"
+            // action="/thank-you" 
+            // functioniert nicht mit React, aber mit next.js 
+            >
                 <input type="hidden" name="form-name" value="contact" />
 
                 Name: <input type="text" name="name" />
